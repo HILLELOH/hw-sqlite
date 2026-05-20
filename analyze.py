fe9bcd7b8c793fe1111b576c6404b7305a21411f
Erel Segal-Halevi <erelsgl@gmail.com>
2026-05-20 14:05:31 +0300

    move homework from last year

---
+++ analyze.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+def net_support_for_candidate1(candidate1:str, candidate2:str)->int:
+    # Your code here
+    pass
+
+def condorcet_winner()->str:
+    # Your code here
+    pass
+
+if __name__ == '__main__':
+    party = input()
+    if party == "condorcet_winner":
+        print(condorcet_winner())
+    else:
+        candidate1,candidate2 = party.split(",")
+        print(net_support_for_candidate1(candidate1,candidate2))
